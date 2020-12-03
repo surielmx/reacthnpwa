d7d52df874cd923ea0ca742e6642d6a16aa4c7bf
Suriel <pokazzam@gmail.com>
2020-12-02 20:52:00 -0600

    Updated toggle-button test

--- cypress/integration/toggle-theme_spec.js
+++ cypress/integration/toggle-theme_spec.js
@@ -29,21 +29,21 @@ describe('Theme mode', function () {
 
 	it('Toggle from dark to light mode', function () {
 		visit(true);
-		cy.get('body').should('have.class', currentMode);
-		cy.get('body').not(`.${notCurrentMode}`);
+		cy.get('body').should('have.class', 'dark');
+		cy.get('body').not('.light');
 		cy.get('#toggle-theme').should('not.be.visible').should(beChecked);
 		cy.get('.switch-wrapper').click();
 
 		cy.get('#toggle-theme').should('not.be.visible').should('not.be.checked');
-		cy.get('body').not(`.${currentMode}`);
-		cy.get('body').should('have.class', notCurrentMode);
-		cy.getLocalStorage('theme').should('equal', notCurrentMode);
+		cy.get('body').not('.dark');
+		cy.get('body').should('have.class', 'light');
+		cy.getLocalStorage('theme').should('equal', 'light');
 	});
 
 	it('Toggle from light to dark mode', function () {
 		visit(false);
-		cy.get('body').should('have.class', notCurrentMode);
-		cy.get('body').not(`.${currentMode}`);
+		cy.get('body').should('have.class', 'light');
+		cy.get('body').not('.dark');
 		cy.get('#toggle-theme').should('not.be.visible').check({ force: true }).should(beChecked);
 		cy.get('.switch-wrapper').click();
 		cy.get('#toggle-theme')
@@ -51,8 +51,8 @@ describe('Theme mode', function () {
 			.check({ force: true })
 			.should('be.checked');
 
-		cy.get('body').not(`.${notCurrentMode}`);
-		cy.get('body').should('have.class', currentMode);
-		cy.getLocalStorage('theme').should('equal', currentMode);
+		cy.get('body').not('.light');
+		cy.get('body').should('have.class', 'dark');
+		cy.getLocalStorage('theme').should('equal', 'dark');
 	});
 });
